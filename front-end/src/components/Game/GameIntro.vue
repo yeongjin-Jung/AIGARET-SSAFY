3fc3e52c344381b477c300e63e152e30b2fdd75d
hanch <hch9417@naver.com>
2020-09-10 20:00:15 +0900

    Resolves S03P22B203-20

---
+++ front-end/src/components/Game/GameIntro.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>게임 설명 화면</h3>
+    <GamePlay />
+  </div>
+</template>
+
+<script>
+import GamePlay from './GamePlay.vue'
+
+export default {
+  name: 'GameIntro',
+  components: {
+    GamePlay
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front-end/src/components/UserInfomation/ChangeMyPicture.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChangeMyPicture'
+}
+</script>
+
+<style>
+
+</style>
