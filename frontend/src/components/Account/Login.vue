ea9f18b01f5c7cb5d8fa6fc55f0466f3270cd970
hanch <hch9417@naver.com>
2020-09-11 15:42:48 +0900

    vue init & add vuetify

---
+++ frontend/src/components/Account/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div style="width: 400px; height: 70px; margin: auto;">
+        <div style="font-weight: bold; font-size: 36px; text-align: center;" class="">AIGARET</div>
+        <div style="font-weight: bold; font-size: 24px; text-align: center;" class="">AI Game Rehabilitation Trainer</div>
+    </div>
+    <div style="width: 646px; height: 346px; margin: 20px auto; background: black;">
+    </div>
+    <div style="margin: 20px auto;">
+      <div style="display:inline-block; width: 300px; font-size: 16px; color: rgb(255, 255, 255); text-align: center; line-height: 2.5em; border-radius: 4px; background-color: rgb(52, 152, 219);">로그인</div>
+      <div style="display:inline-block; width: 45px;"></div>
+      <div style="display:inline-block; width: 300px; font-size: 16px; color: rgb(64, 64, 64); text-align: center; line-height: 2.5em; border-radius: 4px; background-color: rgb(224, 224, 224);">회원가입</div>
+    </div>
+    <div style="font-size:16px; color: rgb(41, 90, 221); text-decoration:underline;">아이디로 로그인하기</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
---
+++ frontend/src/components/Game/Game.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>왼쪽 Navigation</h2>
+    <GameIntro />
+  </div>
+</template>
+
+<script>
+import GameIntro from './GameIntro.vue'
+
+export default {
+  name: 'Game',
+  components: {
+    GameIntro
+  }
+}
+</script>
+
+<style>
+
+</style>
