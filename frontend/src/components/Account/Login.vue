c66fa2d2c9929aee6f35ac0f0e385c53aa2f76aa
김무성 <ddb8036631@gmail.com>
2020-09-18 02:36:11 +0000

    Resolve S03P22B203-36 "Feature/ signup component"

--- frontend/src/components/Account/Login.vue
+++ frontend/src/components/Account/Login.vue
@@ -1,63 +1,94 @@
 <template>
-  <div>
-    <div style="width: 400px; height: 90px; margin: auto;">
-        <div style="font-weight: bold; font-size: 36px; text-align: center;" class="">AIGARET</div>
-        <div style="font-weight: bold; font-size: 24px; text-align: center;" class="">AI Game Rehabilitation Trainer</div>
-    </div>
+  <v-main id="container" style="background-color: #fcfeff" class="fill-height">
+    <v-container style="height: 100%" class="fill-height">
 
-    <div style="width: 533px; height: 400px; margin: 20px auto; background: black;">
-      <video id="video" height="400" autoplay muted></video>
-    </div>
+      <!-- <div class="fill-height" style="margin: auto;"> -->
+          <v-row>
+            <!-- 로그인 -->
+            <v-col cols="5" height="100%">
+              
+              <v-spacer></v-spacer>
 
-    <div style="width: 533px; margin: 20px auto;">
-      <div style="display:inline-block; width: 250px; font-size: 16px; color: rgb(255, 255, 255); text-align: center; line-height: 2.5em; border-radius: 4px; background-color: #53cde2;">로그인</div>
-      <div style="display:inline-block; width: 33px;"></div>
-      <div style="display:inline-block; width: 250px; font-size: 16px; color: rgb(64, 64, 64); text-align: center; line-height: 2.5em; border-radius: 4px; background-color: rgb(224, 224, 224);" @click="signup()">회원가입</div>
+              <v-form>
+                <h3 style="text-align: center;">AIGARET</h3>
+                <h5 style="text-align: center;">AI Game Rehabilitation Trainer</h5>
+                <v-text-field v-model="loginData.id" dark filled background-color="rgb(52, 63, 87)" label="ID" name="login" style="margin-top: 30px;" append-icon="mdi-account" type="text" @keypress.enter="login(loginData)"></v-text-field>
+                <v-text-field v-model="loginData.password" dark filled background-color="rgb(52, 63, 87)" id="Password" label="Password" name="password" append-icon="mdi-lock" type="password" @keypress.enter="login(loginData)"></v-text-field>
+              </v-form>
+              
+              <div class="float-left">
+                <v-spacer></v-spacer>
+                  <v-btn id="btn-login-id" class="mr-3" @click="login(loginData)">아이디로 로그인</v-btn>
+                  <v-btn id="btn-login-face" class="mr-3" @click="login(loginData)">얼굴인식으로 로그인</v-btn>
+                  <!-- <v-btn id="btn-login-face" class="mr-3" @click="signupDialog = true">회원가입</v-btn> -->
+                  <Signup />
+              </div>
+            
+            </v-col>
 
-      <div style="font-size:16px; color: rgb(41, 90, 221); text-decoration:underline;" @click="loginModal = !loginModal">아이디로 로그인하기</div>
-
-      <v-dialog v-model="loginModal" persistent max-width="500px">
-      <ValidationObserver ref="observer" v-slot="{invalid}">
-        <v-card class="elevation-12">
-          <v-toolbar dark flat>
-            <v-toolbar-title>LogIn</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form>
-
-              <ValidationProvider mode="eager" v-slot="{ errors }" name="Id" rules="required">
-                <v-text-field
-                  v-model="loginData.id"
-                  :error-messages="errors"
-                  label="ID"
-                  name="id"
-                  
-                  prepend-icon="mdi-account"
-                ></v-text-field>
-              </ValidationProvider>
-
-              <ValidationProvider mode="eager" v-slot="{ errors }" name="Password" rules="required">
-                <v-text-field
-                  v-model="loginData.password"
-                  :error-messages="errors"
-                  label="Password"
-                  name="password"
-                  type="password"
-                  prepend-icon="mdi-lock"
-                ></v-text-field>
-              </ValidationProvider>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" @click="loginModal = !loginModal; login(loginData)" :disabled="invalid">Login</v-btn>
-            <v-btn @click="loginModal = !loginModal">Close</v-btn>
-          </v-card-actions>
-        </v-card>
-      </ValidationObserver>
-    </v-dialog>
-    </div>
-  </div>
+
+            <v-col>
+              <div style="float: left; width: 533px; height: 400px; margin: 20px auto; background: black;">
+                <video id="video" height="400" autoplay muted></video>
+              </div>
+            </v-col>
+          </v-row>
+      <!-- </div> -->
+
+      
+
+      <!-- <div style="width: 533px; margin: 20px auto;">
+        <div style="display:inline-block; width: 250px; font-size: 16px; color: rgb(255, 255, 255); text-align: center; line-height: 2.5em; border-radius: 4px; background-color: #53cde2;">로그인</div>
+        <div style="display:inline-block; width: 33px;"></div>
+        <div style="display:inline-block; width: 250px; font-size: 16px; color: rgb(255, 255, 255); text-align: center; line-height: 2.5em; border-radius: 4px; background-color: #005792;" @click="signup()">회원가입</div>
+        <div style="font-size:16px; color: rgb(41, 90, 221); text-decoration:underline;" @click="loginModal = !loginModal">아이디로 로그인하기</div>
+
+        <v-dialog v-model="loginModal" persistent max-width="500px">
+        <ValidationObserver ref="observer" v-slot="{invalid}">
+          <v-card class="elevation-12">
+            <v-toolbar dark flat>
+              <v-toolbar-title>LogIn</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+
+                <ValidationProvider mode="eager" v-slot="{ errors }" name="Id" rules="required">
+                  <v-text-field
+                    v-model="loginData.id"
+                    :error-messages="errors"
+                    label="ID"
+                    name="id"
+                    
+                    prepend-icon="mdi-account"
+                  ></v-text-field>
+                </ValidationProvider>
+
+                <ValidationProvider mode="eager" v-slot="{ errors }" name="Password" rules="required">
+                  <v-text-field
+                    v-model="loginData.password"
+                    :error-messages="errors"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    prepend-icon="mdi-lock"
+                  ></v-text-field>
+                </ValidationProvider>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" @click="loginModal = !loginModal; login(loginData)" :disabled="invalid">Login</v-btn>
+              <v-btn @click="loginModal = !loginModal">Close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </ValidationObserver>
+      </v-dialog>
+      
+      </div> -->
+
+    </v-container>
+  </v-main>
 </template>
 
 <script>
@@ -65,6 +96,8 @@ import { ValidationObserver, ValidationProvider, setInteractionMode, extend } fr
 import { required, email } from 'vee-validate/dist/rules';
 
 import { mapState, mapActions } from 'vuex';
+
+import Signup from '@/components/Account/Signup'
 
 extend('required', {
   ...required,
@@ -77,12 +110,15 @@ export default {
   components: {
     ValidationProvider,
     ValidationObserver,
+    Signup
   },
 
   data () {
     return {
+      signupDialog: false,
+      video: document.getElementById('video'),
+      localStream: "",
       faceapi: null,
-      loginModal: "",
       loginData: {
         id: "",
         password: ""
@@ -98,19 +134,25 @@ export default {
     startVideo (val) {
       navigator.getUserMedia(
         { video: true },
-        stream => { val.srcObject = stream },
+        stream => { val.srcObject = stream; this.localStream = stream },
         err => console.error(err)
       )
     },
 
+    stopVideo(val) {
+      video.pause();
+      this.localStream.getTracks()[0].stop();
+    },
+
     signup() {
+      this.stopVideo(video)
       this.$router.push({name: "Signup"})
     },
   },
 
   mounted () {
-    const video = document.getElementById('video')
-    this.startVideo(video)
+    // const video = document.getElementById('video')
+    // this.startVideo(video)
 
     // const recaptchaScript = document.createElement('script')
     // recaptchaScript.setAttribute('type', 'javascript')
@@ -144,4 +186,43 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Jua&display=swap');
+
+/* html, body {
+  background-color: #d1f4fa
+} */
+
+/* #container {
+  margin:0 auto;
+  background-color: #d1f4fa
+} */
+
+div {
+  font-family: 'Jua', sans-serif;
+  font-size: 30px;
+  color: black;
+  line-height: 1.5;
+}
+
+#btn-login-id {
+  padding: 20px;
+  background-color: #53cde2;
+  color: white;
+  outline:none;
+}
+
+#btn-login-face {
+  padding: 20px;
+  background-color: #005792;
+  color: white;
+  outline:none;
+}
+
+#btn-signup {
+  padding: 20px;
+  background-color: #324ec9;
+  color: white;
+  outline:none;
+}
+
 </style>
