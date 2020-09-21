02ccea1904b5be1d832319a7700b25f605099e30
yeongjin <wjddudwls13@naver.com>
2020-09-22 01:31:34 +0900

    Resolves S03P23B203-9

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <v-app style="background: #FCFCFF">
     <v-main>
-    <!-- <v-main class="pa-4 text-center" style="border: white dashed; background-color: black;"><span style="color: white">App.vue > v-main</span> -->
+      <v-system-bar
+        color="orange"
+        :height="height"
+        :lights-out="lightsOut"
+        :window="window"
+        style="position : absolute; width:100%;"
+      >
+        <img
+          src="https://w7.pngwing.com/pngs/981/645/png-transparent-default-profile-united-states-computer-icons-desktop-free-high-quality-person-icon-miscellaneous-silhouette-symbol.png"
+          width="75"
+          height="75"
+          style="margin-left : 40px;"
+        />
+        <p style="font-size: 50px; font-weight: 600; margin-left: 30px; margin-top:20px;">불타는 붕어빵</p>
+        <v-spacer></v-spacer>
+        <p style="font-size: 50px; font-weight: 600; margin-right: 100px; margin-top:15px;">{{this.date}}</p>
+        <p style="font-size: 50px; font-weight: 600; margin-right: 20px; margin-top:15px;">{{this.dateTime}}</p>
+      </v-system-bar>
+      <!-- <v-main class="pa-4 text-center" style="border: white dashed; background-color: black;"><span style="color: white">App.vue > v-main</span> -->
       <v-container fluid class="fill-height">
         <router-view />
       </v-container>
     </v-main>
-  
+
     <v-footer app padless>
-    <!-- <v-footer app style="border: white dashed; background-color: black;"> -->
-      <v-bottom-navigation
-        :value="activeBtn"
-        color="#53cde2"
-        horizontal
-      >
-        <v-btn
-          v-for="(icon, index) in icons"
-          :key="index"
-          height="56"
-          class="pa-4"
-        >
+      <!-- <v-footer app style="border: white dashed; background-color: black;"> -->
+      <v-bottom-navigation :value="activeBtn" color="#53cde2" horizontal>
+        <v-btn v-for="(icon, index) in icons" :key="index" height="56" class="pa-4">
           <h2>{{ icon.title }}</h2>
           <v-icon large>{{ icon.img }}</v-icon>
         </v-btn>
@@ -32,28 +41,65 @@
 // import Login from '@/components/Account/Login'
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     // Login
   },
 
   data: () => ({
+    date: null,
+    dateTime: null,
+    height: 90,
+    lightsOut: false,
+    window: false,
+    timestamp: "",
     activeBtn: 1,
     icons: [
       {
-        title: 'Profile',
-        img: 'mdi-card-account-details',
+        title: "Profile",
+        img: "mdi-card-account-details",
       },
       {
-        title: 'Report',
-        img: 'mdi-history',
+        title: "Report",
+        img: "mdi-history",
       },
       {
-        title: 'Settings',
-        img: 'mdi-application-cog',
+        title: "Settings",
+        img: "mdi-application-cog",
       },
     ],
-  })
-}
+  }),
+
+  created() {
+    setInterval(this.getNow, 1000);
+  },
+
+  methods: {
+    getNow: function () {
+      const today = new Date();
+
+      this.date =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
+
+      var hour = 0;
+      if (today.getHours() >= 12) {
+        hour = today.getHours() - 12;
+        const time = hour + ":" + today.getMinutes();
+        const dateTime = time + " pm";
+        this.dateTime = dateTime;
+      } else if (today.getHours() < 12) {
+        hour = today.getHours();
+        const time = hour + ":" + today.getMinutes();
+        const dateTime = time + " am";
+        this.dateTime = dateTime;
+      }
+    },
+  },
+};
 </script>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,143 +1,129 @@
 <template>
   <v-container fluid class="text-center">
-
-  <!-- <v-container class="pa-4 text-center" style="border: white dashed"><span style="color: white">Home.vue</span> -->
+    <!-- <v-container class="pa-4 text-center" style="border: white dashed"><span style="color: white">Home.vue</span> -->
     <!-- <v-row align="center" justify="center"> -->
-      <!-- <v-sheet class="mx-auto" elevation="8" max-width="800"> -->
-        <v-slide-group v-model="model" show-arrows center-active>
-          <v-slide-item v-for="(item, i) in items" :key="i" v-slot:default="{ active, toggle }">
-            <v-col cols="2">
-              <v-hover v-slot:default="{ hover }">
-                <v-card
-                  :elevation="hover ? 12 : 2"
-                  :class="{ 'on-hover': hover, 'on-active': active, 'on-minimize': model != null }"
-                  @click="toggle"
-                >
-                  <v-img
-                    :src="item.img"
-                    width="100%"
-                    height="100%"
-                  >
-                    <v-card-title class="title white--text">
-                      <v-row
-                        class="fill-height flex-column"
-                        justify="space-between"
+    <!-- <v-sheet class="mx-auto" elevation="8" max-width="800"> -->
+     
+    <p style="font-size: 50px; font-weight: 600; position: relative; left: -800px;">
+      <MARQUEE scrollamount="13" width="500px" height="100%">게임 제목을 입력해 주세요</MARQUEE>
+      <!-- 크기 조절 -->
+    </p>
+    <v-slide-group v-model="model" show-arrows center-active>
+      <v-slide-item v-for="(item, i) in items" :key="i" v-slot:default="{ active, toggle }">
+        <v-col cols="2">
+          <v-hover v-slot:default="{ hover }">
+            <v-card
+              :elevation="hover ? 12 : 2"
+              :class="{ 'on-hover': hover, 'on-active': active, 'on-minimize': model != null }"
+              @click="toggle"
+            >
+              <v-img :src="item.img" width="100%" height="100%">
+                <v-card-title class="title white--text">
+                  <v-row class="fill-height flex-column" justify="space-between">
+                    <p class="mt-4 subheading text-left">{{ item.title }}</p>
+
+                    <div>
+                      <p class="ma-0 body-1 font-weight-bold font-italic text-left">{{ item.text }}</p>
+                      <p class="caption font-weight-medium font-italic text-left">{{ item.subtext }}</p>
+                    </div>
+
+                    <div class="align-self-center">
+                      <v-btn
+                        v-for="(icon, index) in icons"
+                        :key="index"
+                        :class="{ 'show-btns': hover }"
+                        color="transparent"
+                        icon
                       >
-                        <p class="mt-4 subheading text-left">{{ item.title }}</p>
+                        <v-icon :class="{ 'show-btns': hover }" color="transparent">{{ icon }}</v-icon>
+                      </v-btn>
+                    </div>
+                  </v-row>
+                </v-card-title>
+              </v-img>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-slide-item>
+    </v-slide-group>
 
-                        <div>
-                          <p class="ma-0 body-1 font-weight-bold font-italic text-left">
-                            {{ item.text }}
-                          </p>
-                          <p class="caption font-weight-medium font-italic text-left">
-                            {{ item.subtext }}
-                          </p>
-                        </div>
-
-                        <div class="align-self-center">
-                          <v-btn
-                            v-for="(icon, index) in icons"
-                            :key="index"
-                            :class="{ 'show-btns': hover }"
-                            color="transparent"
-                            icon
-                          >
-                            <v-icon
-                              :class="{ 'show-btns': hover }"
-                              color="transparent"
-                            >
-                              {{ icon }}
-                            </v-icon>
-                          </v-btn>
-                        </div>
-                      </v-row>
-                    </v-card-title>
-                  </v-img>
-                </v-card>
-              </v-hover>
-            </v-col>
-          </v-slide-item>
-        </v-slide-group>
-        
-        <v-expand-transition>
-          <v-sheet
-            v-if="model != null"
-            color="grey lighten-4"
-            height="500"
-            tile
-          >
-            <!-- <v-row
+    <v-expand-transition>
+      <v-sheet v-if="model != null" color="grey lighten-4" height="500" tile>
+        <!-- <v-row
               class="fill-height"
               align="center"
               justify="center"
-            > -->
-              <h3 class="title">Selected {{ model+1 }}</h3>
-              <GameIntro />
-            <!-- </v-row> -->
-          </v-sheet>
-        </v-expand-transition>
-      <!-- </v-sheet> -->
+        >-->
+        <h3 class="title">Selected {{ model+1 }}</h3>
+        <GameIntro />
+        <!-- </v-row> -->
+      </v-sheet>
+    </v-expand-transition>
+    <!-- </v-sheet> -->
     <!-- </v-row> -->
   </v-container>
 </template>
 
 <script>
-import GameIntro from '../components/Game/GameIntro.vue'
+import GameIntro from "../components/Game/GameIntro.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    GameIntro
+    GameIntro,
   },
   data: () => ({
     model: null,
-    icons: ['mdi-rewind', 'mdi-play', 'mdi-fast-forward'],
+    icons: ["mdi-rewind", "mdi-play", "mdi-fast-forward"],
     items: [
       {
-        title: 'New Releases',
+        title: "New Releases",
         text: "It's New Release Friday",
-        subtext: 'Newly released songs. Updated daily.',
-        img: 'http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg',
+        subtext: "Newly released songs. Updated daily.",
+        img: "http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg",
       },
       {
-        title: 'Rock',
-        text: 'Greatest Rock Hits',
-        subtext: 'Lose yourself in rock tunes.',
-        img: 'https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+        title: "Rock",
+        text: "Greatest Rock Hits",
+        subtext: "Lose yourself in rock tunes.",
+        img:
+          "https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
       },
       {
-        title: 'Mellow Moods',
-        text: 'Ambient Bass',
-        subtext: 'Chill beats to mellow you out.',
-        img: 'http://lorempixel.com/output/abstract-q-c-640-480-6.jpg',
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "http://lorempixel.com/output/abstract-q-c-640-480-6.jpg",
       },
       {
-        title: 'New Releases',
+        title: "New Releases",
         text: "It's New Release Friday",
-        subtext: 'Newly released songs. Updated daily.',
-        img: 'http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg',
-      },
-            {
-        title: 'Rock',
-        text: 'Greatest Rock Hits',
-        subtext: 'Lose yourself in rock tunes.',
-        img: 'https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+        subtext: "Newly released songs. Updated daily.",
+        img: "http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg",
       },
       {
-        title: 'Mellow Moods',
-        text: 'Ambient Bass',
-        subtext: 'Chill beats to mellow you out.',
-        img: 'http://lorempixel.com/output/abstract-q-c-640-480-6.jpg',
+        title: "Rock",
+        text: "Greatest Rock Hits",
+        subtext: "Lose yourself in rock tunes.",
+        img:
+          "https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "http://lorempixel.com/output/abstract-q-c-640-480-6.jpg",
       },
     ],
-    transparent: 'rgba(255, 255, 255, 0)',
+    transparent: "rgba(255, 255, 255, 0)",
   }),
-}
+};
 </script>
 
 <style scoped>
 .v-card {
-  transition: opacity .4s ease-in-out, width .1s ease-in-out, height .2s ease-in-out;
+  transition: opacity 0.4s ease-in-out, width 0.1s ease-in-out,
+    height 0.2s ease-in-out;
   width: 252px;
   height: 126px;
 }
