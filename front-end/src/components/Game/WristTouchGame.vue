636492f661f0a2a21878fe57a0b84024ecf7d0ce
yeongjin <wjddudwls13@naver.com>
2020-09-23 15:45:57 +0900

    game loading css

--- front-end/src/components/Game/WristTouchGame.vue
+++ front-end/src/components/Game/WristTouchGame.vue
@@ -8,9 +8,13 @@
       <!-- /default -->
       <!-- /footer -->
       <template slot="footer" style="background-color : rgba(0,0,0,1);">
-        <button @click="doClose" style="background-color : red; height :8vh; border-radius: 12px;  width:10vw; font-size : 4vh; font-weight: 600; color:yellow;">다시시작</button>
+        <button
+          @click="doClose"
+          style="background-color : red; height :8vh; border-radius: 12px;  width:10vw; font-size : 4vh; font-weight: 600; color:yellow;"
+        >다시시작</button>
       </template>
     </GameFinishModal>
+    <Loading v-if="loading"></Loading>
   </div>
 </template>
 
@@ -19,6 +23,9 @@ import VueP5 from "vue-p5";
 import ml5 from "ml5";
 import $ from "jquery";
 import GameFinishModal from "./GameFinishModal";
+import Loading from "./loding";
+
+
 export default {
   name: "WristTouchGame",
   data: function () {
@@ -33,18 +40,22 @@ export default {
       window_height: 700,
       countDown: 10,
       modal: false,
-      score : 0,
+      score: 0,
+      mouse: null,
+      loading : false,
     };
   },
   components: {
     VueP5,
     GameFinishModal,
+    Loading,
   },
   methods: {
     setup(sketch) {
       sketch.createCanvas(this.window_width, this.window_height);
       sketch.background(0);
       this.video = sketch.createCapture(sketch.VIDEO);
+
       this.video.size(this.window_width, this.window_height);
       this.video.hide();
       this.poseNet = ml5.poseNet(this.video, this.modelReady());
@@ -55,6 +66,11 @@ export default {
           that.skeleton = results[0].skeleton;
         }
       });
+
+      this.train = sketch.createImg(
+        "http://www.memozee.com/FILES/047/jinsuk.729.%ED%8F%AC%EC%BC%93%EB%AA%AC.jpg"
+      );
+      this.train.hide();
 
       $("#defaultCanvas0").parent().css({
         width: "100vw",
@@ -91,88 +107,94 @@ export default {
       sketch.scale(-1, 1);
       sketch.image(this.video, 0, 0, this.window_width, this.window_height);
 
-      //장애물 
-      sketch.rect(this.position_x, this.position_y, 100, 100);
-      sketch.translate(this.window_width, 0);
-      sketch.scale(-1, 1);
-
-      //점수판
-      sketch.textSize(50);
-      sketch.textStyle(sketch.BOLD);
-      sketch.fill(255, 0, 0);
-      sketch.text("점수 : " + this.score, 40,60);
-
-      //카운트 다운 
-      sketch.textSize(100);
-      sketch.textStyle(sketch.NORMAL);
-      sketch.fill(0, 255, 255);
-      sketch.text(this.countDown, 460, 100);
-
-      //전체 Canvas 반전
-      sketch.translate(this.window_width, 0);
-      sketch.scale(-1, 1);
-
-      if (this.countDown == 0) {
-        this.modal = true;
-      }
       var that = this;
-      if (this.pose.score > 0.25 && this.pose != null) {
-        let eyeR = that.pose.rightEye;
-        let eyeL = that.pose.leftEye;
-        let d = sketch.dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
-        sketch.fill(255, 0, 0);
-        sketch.ellipse(that.pose.nose.x, that.pose.nose.y, d);
-        sketch.fill(0, 0, 255);
-
-        if (
-          (that.pose.rightWrist.x > that.position_x &&
-            that.pose.rightWrist.x < that.position_x + 100 &&
-            that.pose.rightWrist.y > that.position_y &&
-            that.pose.rightWrist.y < that.position_y + 100) ||
-          (that.pose.leftWrist.x > that.position_x &&
-            that.pose.leftWrist.x < that.position_x + 100 &&
-            that.pose.leftWrist.y > that.position_y &&
-            that.pose.leftWrist.y < that.position_y + 100)
-        ) {
-          that.chanege();
-        }
-
-        sketch.ellipse(that.pose.rightWrist.x, that.pose.rightWrist.y, 32);
-        sketch.ellipse(that.pose.leftWrist.x, that.pose.leftWrist.y, 32);
-
-        for (let i = 0; i < that.pose.keypoints.length; i++) {
-          let x = that.pose.keypoints[i].position.x;
-          let y = that.pose.keypoints[i].position.y;
-          sketch.fill(0, 255, 0);
-          sketch.ellipse(x, y, 16, 16);
-        }
-
-        for (let i = 0; i < that.skeleton.length; i++) {
-          let a = that.skeleton[i][0];
-          let b = that.skeleton[i][1];
-          sketch.line(a.position.x, a.position.y, b.position.x, b.position.y);
-        }
-      } else {
-        sketch.image(this.video, 0, 0, this.window_width, this.window_height);
+      if (this.pose != null) {
+        this.loading = false;
+        //장애물
         sketch.rect(this.position_x, this.position_y, 100, 100);
         sketch.translate(this.window_width, 0);
         sketch.scale(-1, 1);
-        
+
+        //점수판
+        sketch.textSize(50);
+        sketch.textStyle(sketch.BOLD);
+        sketch.fill(255, 0, 0);
+        sketch.text("점수 : " + this.score, 40, 60);
+
+        //카운트 다운
+        sketch.textSize(100);
+        sketch.textStyle(sketch.NORMAL);
+        sketch.fill(0, 255, 255);
         sketch.text(this.countDown, 460, 100);
+
+        //전체 Canvas 반전
+        sketch.translate(this.window_width, 0);
+        sketch.scale(-1, 1);
+
+        if (this.countDown == 0) {
+          this.modal = true;
+          this.score = 0;
+        }
+
+        if (this.pose.score > 0.25 && this.pose != null) {
+          let eyeR = that.pose.rightEye;
+          let eyeL = that.pose.leftEye;
+          let d = sketch.dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+          sketch.fill(255, 0, 0);
+          sketch.ellipse(that.pose.nose.x, that.pose.nose.y, d);
+          sketch.fill(0, 0, 255);
+
+          if (
+            (that.pose.rightWrist.x > that.position_x &&
+              that.pose.rightWrist.x < that.position_x + 100 &&
+              that.pose.rightWrist.y > that.position_y &&
+              that.pose.rightWrist.y < that.position_y + 100) ||
+            (that.pose.leftWrist.x > that.position_x &&
+              that.pose.leftWrist.x < that.position_x + 100 &&
+              that.pose.leftWrist.y > that.position_y &&
+              that.pose.leftWrist.y < that.position_y + 100)
+          ) {
+            that.chanege();
+          }
+
+          sketch.ellipse(that.pose.rightWrist.x, that.pose.rightWrist.y, 32);
+          sketch.ellipse(that.pose.leftWrist.x, that.pose.leftWrist.y, 32);
+
+          for (let i = 0; i < that.pose.keypoints.length; i++) {
+            let x = that.pose.keypoints[i].position.x;
+            let y = that.pose.keypoints[i].position.y;
+            sketch.fill(0, 255, 0);
+            sketch.ellipse(x, y, 16, 16);
+          }
+
+          for (let i = 0; i < that.skeleton.length; i++) {
+            let a = that.skeleton[i][0];
+            let b = that.skeleton[i][1];
+            sketch.line(a.position.x, a.position.y, b.position.x, b.position.y);
+          }
+        } else {
+          sketch.image(this.video, 0, 0, this.window_width, this.window_height);
+          sketch.rect(this.position_x, this.position_y, 100, 100);
+          sketch.translate(this.window_width, 0);
+          sketch.scale(-1, 1);
+
+          sketch.text(this.countDown, 460, 100);
+        }
+      } else {
+        this.loading = true;
+        console.log("로딩중");
       }
     },
     closeModal() {
       this.modal = false;
     },
 
-    
-
-    doClose(){
+    doClose() {
       this.score = 0;
       this.countDown = 10;
       this.countDownTimer();
       this.closeModal();
-    }
+    },
   },
   created() {
     this.countDownTimer();
@@ -180,7 +202,8 @@ export default {
 };
 </script>
 
-<style>
+
+<style lang="scss" scoped>
 #defaultCanvas0 {
   display: inline-block;
   height: 70vh;
