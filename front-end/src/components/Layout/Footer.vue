81b6dd43f72e9fddb566480794a82681b4d5788c
kimmuseong <ddb8036631@gmail.com>
2020-09-24 01:19:37 +0900

    Resolves S03P23B203-16 / 네브, 푸터 컴포넌트화

---
+++ front-end/src/components/Layout/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-footer app padless style="height : 8vh;">
+    <!-- <v-footer app style="border: white dashed; background-color: black;"> -->
+    <v-bottom-navigation :value="activeBtn" color="#53cde2" horizontal style="height : 8vh;">
+      <router-link
+        v-for="(icon, index) in icons"
+        :key="index"
+        :to="icon.url"
+        style="text-decoration: none; color: #53cde2; height : 8vh;"
+      >
+        <v-btn style="height: 8vh;">
+          <p style="font-size: 3vh; margin-top : 10px; ">{{ icon.title }}</p>
+          <v-icon style="font-size: 5vh;">{{ icon.img }}</v-icon>
+        </v-btn>
+      </router-link>
+    </v-bottom-navigation>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+
+  data() {
+    return {
+      activeBtn: 1,
+      icons: [
+        {
+          title: "Profile",
+          img: "mdi-card-account-details",
+          url: "info"
+        },
+        {
+          title: "Report",
+          img: "mdi-history",
+          url: "report"
+        },
+        {
+          title: "Settings",
+          img: "mdi-application-cog",
+          url: "/"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ front-end/src/components/Layout/Nav.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-system-bar
+    color="orange"
+    :lights-out="lightsOut"
+    :window="window"
+    style="position : absolute; width:100%; height : 7vh"
+  >
+    <img
+      src="https://w7.pngwing.com/pngs/981/645/png-transparent-default-profile-united-states-computer-icons-desktop-free-high-quality-person-icon-miscellaneous-silhouette-symbol.png"
+      style="margin-left : 40px; width : 3vw; height: 5vh;"
+    />
+    <p
+      style="font-size: 3.5vh; white-space:nowrap; font-weight: 600; margin-left: 30px; margin-top:15px; display:inline; font-family: CookieRun-Bold;"
+    >불타는 붕어빵</p>
+    <v-spacer></v-spacer>
+    <p
+      style="font-size: 3.5vh; white-space:nowrap; font-weight: 600; margin-right: 100px; margin-top:15px;"
+    >{{this.date}}</p>
+    <p
+      style="font-size: 3.5vh; white-space:nowrap; font-weight: 600; margin-right: 20px; margin-top:15px;"
+    >{{this.dateTime}}</p>
+  </v-system-bar>
+  <!-- <v-main class="pa-4 text-center" style="border: white dashed; background-color: black;"><span style="color: white">App.vue > v-main</span> -->
+</template>
+
+<script>
+export default {
+  name: "Nav",
+
+  data() {
+    return {
+      window: "",
+      date: null,
+      dateTime: null,
+      lightsOut: false
+    };
+  },
+
+  methods: {
+    getNow: function() {
+      const today = new Date();
+
+      this.date =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
+
+      var hour = 0;
+      var minute = 0;
+      if (today.getHours() > 12) {
+        hour = today.getHours() - 12;
+        if (today.getMinutes() < 10) {
+          minute = "0" + today.getMinutes();
+        } else {
+          minute = today.getMinutes();
+        }
+        const time = hour + ":" + minute;
+        const dateTime = time + " pm";
+        this.dateTime = dateTime;
+      } else if (today.getHours() < 12) {
+        hour = today.getHours();
+        if (today.getMinutes() < 10) {
+          minute = "0" + today.getMinutes();
+        } else {
+          minute = today.getMinutes();
+        }
+        const time = hour + ":" + minute;
+        const dateTime = time + " am";
+        this.dateTime = dateTime;
+      } else {
+        hour = today.getHours();
+        if (today.getMinutes() < 10) {
+          minute = "0" + today.getMinutes();
+        } else {
+          minute = today.getMinutes();
+        }
+        const time = hour + ":" + minute;
+        const dateTime = time + " pm";
+        this.dateTime = dateTime;
+      }
+    }
+  },
+
+  created() {
+    setInterval(this.getNow, 1000);
+  }
+};
+</script>
+
+<style>
+</style>
