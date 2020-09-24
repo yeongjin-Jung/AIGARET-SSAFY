41f4bf522aec187a28545e5ac10c054ea1c4be90
unknown <ursopretty93@naver.com>
2020-09-24 21:07:23 +0900

    game authorization

--- back-end/games/views.py
+++ back-end/games/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Game
 from .serializers import GameSerializer
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticatedOrReadOnly])
+@permission_classes([IsAuthenticated])
 def detail(request, game_pk):
     game = get_object_or_404(Game, pk=game_pk)
     serializer = GameSerializer(game)
