7654cb594482ab9b0daeb3c422dd71c1811183dc
hanch <hch9417@naver.com>
2020-10-03 21:41:26 +0900

    btn css

--- front-end/src/components/Layout/Nav.vue
+++ front-end/src/components/Layout/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <v-system-bar app color="orange" style="height: 5vh;">
     <router-link to="/" style="text-decoration: none;">
-      <v-btn icon tile style="margin-left: 10px; height:3.5vh; margin-top: 1px; margin-left: 10px;"><v-icon style="font-size: 5vh;" >mdi-home</v-icon></v-btn>
+      <v-btn icon tile style="width: 5vh; height: 5vh;"><v-icon style="font-size: 5vh;" >mdi-home</v-icon></v-btn>
     </router-link>
     <p
       style="font-size: 4.4vh; white-space:nowrap; font-weight: 600; margin-top: 16px; margin-left: 10px; display:inline; font-family: CookieRun-Bold;"
