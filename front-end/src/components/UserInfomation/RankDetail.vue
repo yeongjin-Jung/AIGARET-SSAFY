58a7f36e45c6ba0474fa22e772d2969f7a386dfa
hanch <hch9417@naver.com>
2020-10-04 17:21:19 +0900

    RankPage -ing

--- front-end/src/components/UserInfomation/RankDetail.vue
+++ front-end/src/components/UserInfomation/RankDetail.vue
@@ -1,50 +1,28 @@
 <template>
-  <v-row no-gutters class="fill-height text-center">
+  <v-row no-gutters class="fill-height text-center" justify="space-around">
+    <!-- <v-col id="rankboard" cols="5" style="border: green 3px dashed;"> -->
     <v-col cols="5" align-self="center"> <!-- 2분할 -->
-      <v-row no-gutters justify="center"> <!-- 4분할 -->
-        <v-col cols="6" style="display: flex; align-items: center; justify-content: center; height: 40vh;">
-        <!-- cols="6" 으로 가운데 정렬 효과 -->
-          
-          <v-col style="border: black 3px dashed;"> <!-- v-col > v-col, 횡 => 종 변화 -->
-          <!-- <v-row> 주석풀어보면 횡 변화함 알 수 있음 -->
-
-            <v-col style="border: black 3px dashed;">
-              <!-- <div style="border: black 3px dashed;">주간최고점수</div> -->
-              <!-- div 대신 v-col 로 써도됨(line.16 주석 풀어볼 것), 상황에따라 횡,종 변화할 수 있음 (line.10, 22 주석풀어볼 것) -->
-              <v-col style="border: red 3px dashed;">
-                주간최고점수(선택한게임의 이번주 나의 최고점수)
-                <!-- <div style="border: black 3px dashed;">{{ myDataWeek[0].score }}</div> -->
-                <div v-for="n in myDataWeek.length" :key="n" style="border: black 3px dashed;">{{ myDataWeek[n-1].score }}</div>
-              </v-col>
-              <!-- <v-col style="border: black 3px dashed;">주간최고점수</v-col> -->
-            </v-col>
-          
-          <!-- </v-row> -->
-          </v-col>
-        
+      <v-col id="rankboard" style="height: 80vh;">
+        <v-col style="width: 25vw; margin: 15vh auto 0; display: flex; justify-content: flex-end;">
+          <div style="font-family: CookieRun-Bold">AIGARET 최고점수: {{ gameData[0].score }}점 ({{ gameData[0].user }})</div>
         </v-col>
-      </v-row>
-      <v-row no-gutters justify="center"> <!-- 4분할 -->
-        <v-col cols="6" style="display: flex; align-items: center; justify-content: center; height: 40vh;">
-        <!-- cols="6" 으로 가운데 정렬 효과 -->
-          <div style="border: black 3px dashed;">
-            최고점수(선택한게임의 역대 나의 최고점수)
-            <div v-for="n in myData.length" :key="n" style="border: black 3px dashed;">{{ myData[n-1].score }}</div>
-          </div>
-        </v-col>
-      </v-row>
-    </v-col>
-
-    <v-col cols="7" align-self="center" style="border: green 3px dashed;"> <!-- 2분할 -->
-          <v-col style="border: red 3px dashed; display: flex; align-items: center; justify-content: space-around;">
-            전체랭킹(선택한게임의 이번주 최고점수)
-            <div style="border: black 3px dashed;">이번주 최고점수: {{ gameData[0].score }}</div>
+        <!-- <v-row no-gutters justify="center"> -->
+        <v-col id="rankdiv" v-for="n in gameDataWeek.length" :key="n" style="width: 25vw; margin: 0 auto; display: flex; justify-content: space-around;">
+          <!-- <v-col  cols="1"></v-col> -->
+          <v-col cols="2">
+            <div id="rankfont">{{ n }}위</div>
           </v-col>
-      <!-- <v-row no-gutters justify="center">
-        <v-col style="border: gray 3px dashed; display: flex; align-items: center; justify-content: center;"> -->
-            <div v-for="n in gameData.length" :key="n" style="border: black 3px dashed; display: flex; justify-content: space-around;"><div>{{ gameData[n-1].user }}님</div> <div>Score: {{ gameData[n-1].score }}</div></div>
-        <!-- </v-col>
-      </v-row> -->
+          <v-col cols="5">
+            <div id="rankfont">{{ gameDataWeek[n-1].user }}님</div>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="4">
+            <div id="rankfont">{{ gameDataWeek[n-1].score }}점</div>
+          </v-col>
+          <!-- <v-col  cols="1"></v-col> -->
+        </v-col>
+        <!-- </v-row> -->
+      </v-col>
     </v-col>
   </v-row>
 </template>
@@ -58,8 +36,9 @@ export default {
   data() {
     return {
       gameData: [{}],
-      myData: {},
-      myDataWeek: {},
+      gameDataWeek: [{}],
+      myData: [{}],
+      myDataWeek: [{}],
     }
   },
   props: {
@@ -70,12 +49,28 @@ export default {
       axios.get(SERVER.URL + `games/${ this.tabIndex }/records/`, {
         params: {
           sort: 'high',
-          count: 10,
+          count: 1,
           week: false
         },
       }).then((res) => {
           if (res.status === 200) {
             this.gameData = res.data
+          } else {
+            console.log('ErrorNo: ', res.status);
+          }
+        })
+        .catch((err) => console.log(err));
+    },
+    getGameDataWeek() {
+      axios.get(SERVER.URL + `games/${ this.tabIndex }/records/`, {
+        params: {
+          sort: 'high',
+          count: 5,
+          week: true
+        },
+      }).then((res) => {
+          if (res.status === 200) {
+            this.gameDataWeek = res.data
           } else {
             console.log('ErrorNo: ', res.status);
           }
@@ -117,14 +112,27 @@ export default {
   },
   mounted() {
     this.getGameData()
+    this.getGameDataWeek()
     this.getMyGameData()
     this.getMyGameDataWeek()
   },
-  updated() {
-  }
 }
 </script>
 
 <style>
+#rankboard {
+  background-image: url("../../assets/rank/Rankboard.png");
+  background-size : 40vw 80vh;
+}
 
+#rankdiv {
+  background-image: url("../../assets/rank/Rankdiv.png");
+  background-size : 25.5vw 10vh;
+}
+
+#rankfont {
+  white-space: nowrap;
+  font-family: CookieRun-Bold;
+  font-size: 2.5vh;
+}
 </style>
