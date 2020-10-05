c34ff05d726b520f922313736f28e2a19c4540fc
yeongjin <wjddudwls13@naver.com>
2020-10-05 17:14:10 +0900

    차트 글씨 변경

--- front-end/src/components/Account/PieChart.vue
+++ front-end/src/components/Account/PieChart.vue
@@ -1,6 +1,6 @@
 <script>
-import axios from 'axios'
-import SERVER from '@/api/server.js'
+import axios from "axios";
+import SERVER from "@/api/server.js";
 
 import { Pie } from "vue-chartjs";
 export default {
@@ -15,46 +15,62 @@ export default {
             borderColor: [
               "rgba(255,99,132,1)",
               "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)"
+              "rgba(75, 192, 192, 1)",
             ],
             backgroundColor: [
               "rgba(255, 99, 132, 1)",
               "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)"
+              "rgba(75, 192, 192, 1)",
             ],
-            data: []
+            data: [],
           },
-        ]
+        ],
       },
       options: {
         legend: {
-          display: true
+          display: true,
+          labels: {
+            // This more specific font property overrides the global property
+            fontColor: "black",
+            fontSize : 15,
+            fontStyle: "bold",
+          },
         },
         responsive: true,
-        maintainAspectRatio: false
-      }
+        maintainAspectRatio: false,
+      },
     };
   },
 
   mounted() {
-    axios.post(SERVER.URL + SERVER.ROUTES.getPieData)
-      .then(res => {
-        console.log("pie chart response : ", res)
+    axios
+      .post(SERVER.URL + SERVER.ROUTES.getPieData)
+      .then((res) => {
+        console.log("pie chart response : ", res);
 
-        console.log("손목 터치 게임 : ", res.data.WristTouchGame.play_time__sum)
-        console.log("스네이크 게임 : ", res.data.SnakeGame.play_time__sum)
-        console.log("점프 게임 : ", res.data.JumpGame.play_time__sum)
+        console.log(
+          "손목 터치 게임 : ",
+          res.data.WristTouchGame.play_time__sum
+        );
+        console.log("스네이크 게임 : ", res.data.SnakeGame.play_time__sum);
+        console.log("점프 게임 : ", res.data.JumpGame.play_time__sum);
 
-        this.chartData.datasets[0].data.push(parseInt(res.data.WristTouchGame.play_time__sum / 60))
-        this.chartData.datasets[0].data.push(parseInt(res.data.SnakeGame.play_time__sum / 60))
-        this.chartData.datasets[0].data.push(parseInt(res.data.JumpGame.play_time__sum / 60))
+        this.chartData.datasets[0].data.push(
+          parseInt(res.data.WristTouchGame.play_time__sum / 60)
+        );
+        this.chartData.datasets[0].data.push(
+          parseInt(res.data.SnakeGame.play_time__sum / 60)
+        );
+        this.chartData.datasets[0].data.push(
+          parseInt(res.data.JumpGame.play_time__sum / 60)
+        );
 
         this.renderChart(this.chartData, this.options);
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 };
 </script>
 
