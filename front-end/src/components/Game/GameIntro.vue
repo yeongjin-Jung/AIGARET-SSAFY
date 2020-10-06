4a5b27dcd8aab0c9d6f678b3aad0e31631af48f5
hanch <hch9417@naver.com>
2020-10-06 21:52:46 +0900

    Resolves S03P23B203-74

--- front-end/src/components/Game/GameIntro.vue
+++ front-end/src/components/Game/GameIntro.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="d-inline-block mx-auto" style="width: 100%; height:45vh;">
-    <v-container fluid style="max-height: 100%;">
-      <v-row style="height: 40vh;" justify="center">
-        <v-col cols="6">
-          <iframe :src="`https://youtube.com/embed/${gameInfo.game_video}`" width="70%" height="100%" allowfullscreen></iframe>
-        </v-col>
+  <v-container fluid class="pt-0">
+    <v-row style="height: 45vh;" justify="center">
+      <v-col cols="6">
+        <iframe :src="`https://youtube.com/embed/${gameInfo.game_video}`" width="78%" height="100%" allowfullscreen></iframe>
+      </v-col>
 
-        <v-col cols="6">
-          <v-row no-gutters style="display: inline-block; vertical-align: middle; background-color: rgba(254,254,255,0.5);">
-            <div style="height: 27vh; font-size: 3.5vh; font-family: CookieRun-Bold">
-              {{ effect }}
-              <br/>
-              {{ indication }}
-            </div>
-          </v-row>
-          <v-row no-gutters class="pt-2" justify="center" align="center">
-            <router-link :to="{ path: `/game${ gameInfo.id }/`, query: { gameNo: gameInfo.id } }" style="text-decoration: none;">
-              <v-btn outlined style="width: 10vw; height 10vh; font-family: CookieRun-Bold; background-color: yellow;">게임시작</v-btn>
-            </router-link>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+      <v-col cols="6">
+        <v-row no-gutters style="background-color: rgba(254,254,255,0.5);">
+          <div style="height: 32vh; font-size: 3.5vh; font-family: CookieRun-Bold">
+            {{ effect }}
+            <br/><br/>
+            {{ indication }}
+          </div>
+        </v-row>
+        <v-row no-gutters class="pt-2" justify="center">
+          <router-link :to="{ path: `/game${ gameInfo.id }/`, query: { gameNo: gameInfo.id } }" style="text-decoration: none;">
+            <v-btn outlined style="width: 10vw; height: 8.5vh; font-family: CookieRun-Bold; background-color: yellow;">게임시작</v-btn>
+          </router-link>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -45,5 +43,4 @@ export default {
     }
   }
 }
-
 </script>
